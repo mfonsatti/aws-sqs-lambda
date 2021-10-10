4ba1a349f46471e3b472789d38eb0d8d1ffef3e9
Matteo Fonsatti <matteo.fonsatti@wyscout.com>
2021-10-10 12:29:27 +0200

    created policy and policy attachment to get SQS permission to the Lambda function

--- iam.tf
+++ iam.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "sqs_reader_role_policy" {
+data "aws_iam_policy_document" "sqs_reader_assume_role_policy" {
   statement {
     actions = [
       "sts:AssumeRole"
@@ -9,18 +9,29 @@ data "aws_iam_policy_document" "sqs_reader_role_policy" {
       identifiers = ["lambda.amazonaws.com"]
     }
   }
+}
+
+data "aws_iam_policy_document" "sqs_reader_policy_document" {
   statement {
-    effect = "Allow"
     actions = [
-      "sqs:ReceiveMessage",
-      "sqs:GetQueueAttributes"
+      "sqs:*"
     ]
+
     resources = [
       aws_sqs_queue.terraform_queue.arn
     ]
   }
 }
 
+resource "aws_iam_policy" "sqs_reader_policy" {
+  policy = data.aws_iam_policy_document.sqs_reader_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "sqs_reader_role_policy_attachment" {
+  role       = aws_iam_role.sqs_reader_role.name
+  policy_arn = aws_iam_policy.sqs_reader_policy.arn
+}
+
 resource "aws_iam_role" "sqs_reader_role" {
-  assume_role_policy = data.aws_iam_policy_document.sqs_reader_role_policy.json
+  assume_role_policy = data.aws_iam_policy_document.sqs_reader_assume_role_policy.json
 }
